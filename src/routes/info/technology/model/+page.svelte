0e3f79171879b9e55ea52b3eb077b5d2afeef8fb
camhpj <cjohnson23@gwu.edu>
2023-04-28 10:48:30 -0400

    added pictures

--- src/routes/info/technology/model/+page.svelte
+++ src/routes/info/technology/model/+page.svelte
@@ -1,4 +1,23 @@
 <script lang="ts">
+    import { ImageLoader } from "carbon-components-svelte"
+    import Enceladus from '$lib/assets/enceladus-architecture.png'
+    import TrainingCurves from '$lib/assets/training-curves.svg'
+
 </script>
 
 <h1>Model - Codename: Enceladus</h1>
+
+<br>
+<br>
+
+<h2>Enceladus Architecture</h2>
+<ImageLoader
+    src={Enceladus}
+/>
+
+<br>
+<br>
+
+<ImageLoader
+    src={TrainingCurves}
+/>
